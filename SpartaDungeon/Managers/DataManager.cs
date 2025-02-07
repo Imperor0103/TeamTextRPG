dc9d51d4f051dd70e82fd658e6ddc33c55b084cd
Imperor <imperor0103@gmail.com>
2025-02-07 15:03:15 +0900

    feat : player in DataManager
    for save and load game

--- SpartaDungeon/Managers/DataManager.cs
+++ SpartaDungeon/Managers/DataManager.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace SpartaDungeon.Managers
 {
+    // 저장, 불러오기
     public class DataManager: Singleton<DataManager>
     {
         // 생성자 만들지 않아도 됨
+        public Player player;   // 저장을 위해 플레이어는 DataManager의 멤버변수 
+
     }
 }
