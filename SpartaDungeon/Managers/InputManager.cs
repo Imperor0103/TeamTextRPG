ee37f0208197690fc98e69c5a0de166c52fc8db8
HoMc1343 <qre123456@gmail.com>
2025-02-07 11:30:30 +0900

    docs: InputManager

---
+++ SpartaDungeon/Managers/InputManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon.Managers
+{
+    public class InputManager: Singleton<ItemManager>
+    {
+        // 생성자 만들지 않아도 됨
+        public static class InputHelper
+        {
+            // 숫자 입력
+            public static int GetValidNumber(string prompt, int min, int max) // (출력, ~부터, ~까지)
+            {
+                int choice;
+                while (true)
+                {
+                    Console.WriteLine(prompt);
+                    Console.Write(">> ");
+                    if (int.TryParse(Console.ReadLine(), out choice) && choice >= min && choice <= max)
+                    {
+                        return choice;
+                    }
+                    Console.WriteLine("잘못된 입력입니다");
+                }
+            }
+
+            // 문자열 입력
+            public static string GetValidString(string prompt)
+            {
+                Console.WriteLine(prompt);
+                Console.Write(">> ");
+                string input = Console.ReadLine()?.Trim();
+                while (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("잘못된 입력입니다");
+                    Console.Write(">> ");
+                    input = Console.ReadLine()?.Trim();
+                }
+                return input;
+            }
+        }
+    }
+}
