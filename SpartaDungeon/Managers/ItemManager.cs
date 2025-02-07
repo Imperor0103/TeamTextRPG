9ff1460cf85c58b2c4d1efc949afbbb384bb5378
Imperor <imperor0103@gmail.com>
2025-02-07 19:44:43 +0900

    feat: check item purchased

--- SpartaDungeon/Managers/ItemManager.cs
+++ SpartaDungeon/Managers/ItemManager.cs
@@ -19,6 +19,7 @@ namespace SpartaDungeon.Managers
             Console.WriteLine("[아이템 목록]\n");
             for (int i = 0; i < ItemManager.Instance.equimentList.Count; i++)
             {
+                // 장착했다면 [E] 출력
                 Console.Write($"이름: {ItemManager.Instance.equimentList[i].name} | ");
                 Console.Write($"종류: {ItemManager.Instance.equimentList[i].itemType} | ");
                 Console.Write($"공격력: {ItemManager.Instance.equimentList[i].attack} | ");
@@ -27,14 +28,22 @@ namespace SpartaDungeon.Managers
                 Console.Write($"mp추가: {ItemManager.Instance.equimentList[i].mp} | ");
                 Console.Write($"{ItemManager.Instance.equimentList[i].description} | ");
                 // 아래의 메세지만 따로 출력하는 메서드 만든다
-                //Console.Write($"{ItemManager.Instance.equimentList[i].price} | ");
                 PrintPrice(ItemManager.Instance.equimentList[i]);
             }
         }
+        //public bool IsEquiped(Equipment item)
+        //{
+        //    if ()
+        //    {
+        //        return true;
+        //    }
+        //    return false;
+        //}
+
         public bool IsOwned(Equipment item)
         {
             // 플레이어의 아이템 리스트에 item이 있는지 확인한다
-            if (ItemManager.Instance.equimentList.Contains(item))
+            if (DataManager.Instance.player.ownedList.Contains(item))
             {
                 return true;
             }
