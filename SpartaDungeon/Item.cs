36014366d28a5254031c35bbc140279cd7e502af
XPOW <zug3049@gmail.com>
2025-02-07 16:50:03 +0900

    docs: Itemmanager
    아이템 매니정

--- SpartaDungeon/Item.cs
+++ SpartaDungeon/Item.cs
@@ -1,14 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SpartaDungeon.Managers;
+using System.ComponentModel;
 
 namespace SpartaDungeon
 {
+    public interface ItemData
+    {
+        string name { get; }
+        int itemType { get; }                                                                                                                              // ALL: 모두 장착가능
+        float attack { get; }
+        float defence { get; }
+        float hp { get; }
+        float mp { get; }
+        string description { get; }
+        int price { get; }
+    }
+
+    public abstract class Equipment : ItemData
+    {
+        public string name { get; set; }
+        public  int itemType { get; set; }                                                                                                                              // ALL: 모두 장착가능
+        public  float attack { get; set; }
+        public  float defence { get; set; }
+        public float hp { get; set; }
+        public float mp { get; set; }
+        public string description { get; set; }
+        public  int price { get; set; }
+    }
+
+    public class Armor : Equipment 
+    {
+        public Armor() 
+        { 
+            name = "존나 쌘 갑옷"; 
+            itemType = 1; 
+            defence = 9999; 
+            description = "날 아무도 막을수 없으셈 ㅋㅋ"; 
+            price = 9999; 
+        } 
+    }
+    
+    /* public enum eItemType
+    {
+        WEAPON = 1,
+        ARMOR = 2,
+    }
 
     public class Item
     {
+        public ItemData itemData;
 
+        public Item()
+        {
+
+        }
+
+        public void SetData(string name, eItemType itemType, eClassType classType, float att, float def, float h, float m, string des, int pr)
+        {
+            itemData.name = name;
+            itemData.itemType = itemType;
+            itemData.classType = classType;
+            itemData.attack = att;
+            itemData.defence = def;
+            itemData.hp = h;
+            itemData.mp = m;
+            itemData.description = des;
+            itemData.price = pr;
+        }
     }
+    */
 }
--- SpartaDungeon/Scenes/StoreScene.cs
+++ SpartaDungeon/Scenes/StoreScene.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpartaDungeon.Managers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace SpartaDungeon.Scenes
 {
     public class StoreScene : BaseScene
     {
+        
         #region 새로운 생성자 만들기 금지
         // 생성자에서는 현재 씬의 이름만 설정한다. 씬에 있는 멤버들의 초기화는 Awake나 Start에서 한다
         public StoreScene(string name) : base(name) { }
@@ -15,15 +17,21 @@ namespace SpartaDungeon.Scenes
         //
         public override void Awake()
         {
-
+            Equipment item = new Armor();
+            ItemManager.Instance.equimentList.Add(item);
         }
         public override void Start()
         {
-
+            for (int i = 0; i < ItemManager.Instance.equimentList.Count; i++)
+            {
+                Console.Write($"{i + 1} ");
+                Console.Write(ItemManager.Instance.equimentList[i].name + " | ");
+                Console.Write(ItemManager.Instance.equimentList[i].defence);
+            }
         }
         public override void Update()
         {
-
+          
         }
     }
 }
