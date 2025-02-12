62b29e7fa7731e5080dd157677a5e006c79c5c04
Imperor <imperor0103@gmail.com>
2025-02-12 20:37:32 +0900

    feat: SkillManager, Skill
    format

---
+++ SpartaDungeon/Managers/SkillManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon.Managers
+{
+    // 스킬 매니저가 가져야 할 것
+    // 플레이어가 배운 스킬 저장: 리스트(또는 배열)
+    public class SkillManager : Singleton<SkillManager>
+    {
+        // 배열에 직접 저장할 것
+        public List<Skill> playerSkillArr = new List<Skill>();
+
+        public void InitSkill(Player player)
+        {
+            // 선택한 플레이어의 스킬들을 미리 만든다
+            // 예시
+            if (player.data.classType == eClassType.WARRIOR)
+            {
+                // 스킬 4개 만들기
+                Skill skill_1 = new Skill(1, "백호참", eClassType.WARRIOR, 10, 10, 10, 10, 10);
+                // skill_2, skill_3, skill_4도 이런식으로
+
+
+            }
+            else if (player.data.classType == eClassType.MAGE)
+            {
+                // 스킬 4개 만들기
+
+
+
+            }
+            else if (player.data.classType == eClassType.ARCHER)
+            {
+                // 스킬 4개 만들기
+
+
+
+            }
+        }
+
+        // 이렇게 직접만들지 말고 아래의 메서드를 통해 인덱스에 넣는다
+        //public Skill skill_1 { get; private set; }    // 공격스킬
+        //public Skill skill_2 { get; private set; }    // 공격력상승
+        //public Skill skill_3 { get; private set; }    // 방어력상승
+        //public Skill skill_4 { get; private set; }    // 체력회복
+
+        // 스킬 배우기 메서드
+        public void LearnSkill(Player player, Skill skill)
+        {
+            // 스킬의 직업과 플레이어 직업을 체크 && 레벨체크(플레이어의 레벨 > 스킬레벨)
+            if (player.data.classType == skill.skillData.ClassType &&
+                player.data.level >= skill.skillData.Level)
+            {
+                // 스킬의 Id를 비교해서 "Id-1" 인덱스에 집어넣는다
+                // 1번 스킬은 0번 인덱스에 저장
+                playerSkillArr[skill.skillData.Id - 1] = skill;
+            }
+        }
+
+        // 스킬 사용하기 메서드(반환형이 있는것과 없는것을 만든다)
+        public float UseSkill_1(Player player)
+        {
+            // 마나 소모
+            player.data.mp -= playerSkillArr[0].skillData.Mp;
+
+            // 공격한다(데미지를 리턴해서 몬스터한테 주면 그게 공격임)
+            float damage = 0;
+            return damage;
+        }
+        public void UseSkill_2(Player player)
+        {
+            // 마나 소모
+            player.data.mp -= playerSkillArr[1].skillData.Mp;
+            // 플레이어의 공격력을 "몇초간" 올린다
+            player.data.attack += playerSkillArr[1].skillData.Attack;
+            // 플레이어의 공격력을 몇초간 올리는 방법
+
+        }
+        public void UseSkill_3(Player player)
+        {
+            // 마나 소모
+            player.data.mp -= playerSkillArr[2].skillData.Mp;
+            // 플레이어의 방어력을 "몇초간" 올린다
+            player.data.attack += playerSkillArr[2].skillData.Defence;
+            // 플레이어의 공격력을 몇초간 올리는 방법
+
+        }
+
+        public void UseSkill_4(Player player)
+        {
+            // 마나 소모
+            player.data.mp -= playerSkillArr[3].skillData.Mp;
+
+            // 플레이의 체력을 회복한다
+            player.data.hp += playerSkillArr[0].skillData.Hp;
+        }
+        // 자료형은 바뀔 수 있음
+    }
+}
---
+++ SpartaDungeon/Skill.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon
+{
+    public struct SkillData
+    {
+        // 스킬이 가져야 할 것들(SkillData)
+        // name 
+        // hp
+        // mp
+        // level(특정 레벨이 되면 배울 수 있다)
+        // 그 외 추가할 것이 있다면 추가할 것
+        // 
+
+        public int Id { get; set; }   // 스킬의 번호(1~4)
+        public string Name { get; set; }
+        public eClassType ClassType { get; set; }    // 배울 수 있는 직업
+        public int Level { get; set; }  // 퀘스트 배우는 레벨
+        public float Attack { get; set; }  // 공격력 올려주는 스킬, 공격 스킬에 사용
+        public float Defence { get; set; }  // 방어력 올려준다
+        public float Hp { get; set; }   // 체력회복
+        public float Mp { get; set; }   // 마나 소모량
+    }
+
+    // 스킬이 가지고 있을 것은 스킬의 자료형밖에 없다
+    // 스킬을 사용하는 메서드는 플레이어 또는 스킬매니저에 구현한다
+    public class Skill
+    {
+        public SkillData skillData;
+        // 구현 시도해볼 스킬
+        // 공격
+        // 공격력 상승
+        // 방어력 상승
+        // 체력 회복
+
+        // 기본 생성자
+        public Skill()
+        {
+            skillData.Id = 0;
+            skillData.Name = "";
+            skillData.ClassType = eClassType.ALL;
+            skillData.Level = 0;
+            skillData.Attack = 0f;
+            skillData.Defence = 0f;
+            skillData.Hp = 0f;
+            skillData.Mp = 0f;
+        }
+
+        public Skill(int i, string n, eClassType t, int l, float a, float d, float h, float m)
+        {
+            skillData.Id = i;
+            skillData.Name = n;
+            skillData.ClassType = t;
+            skillData.Level = l;
+            skillData.Attack = a;
+            skillData.Defence = d;
+            skillData.Hp = h;
+            skillData.Mp = m;
+        }
+    }
+}
